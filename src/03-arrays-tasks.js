1c346fc92002cd000720939baad4542fb29addf4
Vladislav Chervyakov <chervyakovve@mail.ru>
2022-12-06 01:54:14 +0700

    feat: add arrays solutions

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -521,8 +521,15 @@ function distinct(arr) {
  *    "Poland" => ["Lodz"]
  *   }
  */
-function group(/* array, keySelector, valueSelector */) {
-  throw new Error('Not implemented');
+function group(array, keySelector, valueSelector) {
+  return array.reduce((acc, item) => {
+    const newKey = keySelector(item);
+    const newValue = valueSelector(item);
+    const newArray = acc.get(newKey) || [];
+    newArray.push(newValue);
+    acc.set(newKey, newArray);
+    return acc;
+  }, new Map());
 }
 
 
@@ -539,8 +546,8 @@ function group(/* array, keySelector, valueSelector */) {
  *   [[1, 2], [3, 4], [5, 6]], (x) => x     =>   [ 1, 2, 3, 4, 5, 6 ]
  *   ['one','two','three'], (x) => x.split('')  =>   ['o','n','e','t','w','o','t','h','r','e','e']
  */
-function selectMany(/* arr, childrenSelector */) {
-  throw new Error('Not implemented');
+function selectMany(arr, childrenSelector) {
+  return arr.map((item) => childrenSelector(item)).flat(Infinity);
 }
 
 
@@ -556,8 +563,11 @@ function selectMany(/* arr, childrenSelector */) {
  *   ['one','two','three'], [2]       => 'three'  (arr[2])
  *   [[[ 1, 2, 3]]], [ 0, 0, 1 ]      => 2        (arr[0][0][1])
  */
-function getElementByIndexes(/* arr, indexes */) {
-  throw new Error('Not implemented');
+function getElementByIndexes(arr, indexes) {
+  const nestCount = indexes.length;
+  if (nestCount === 1) return arr[indexes[0]];
+  if (nestCount === 2) return arr[indexes[0]][indexes[1]];
+  return arr[indexes[0]][indexes[1]][indexes[2]];
 }
 
 
@@ -579,8 +589,12 @@ function getElementByIndexes(/* arr, indexes */) {
  *   [ 1, 2, 3, 4, 5, 6, 7, 8 ]   =>  [ 5, 6, 7, 8, 1, 2, 3, 4 ]
  *
  */
-function swapHeadAndTail(/* arr */) {
-  throw new Error('Not implemented');
+function swapHeadAndTail(arr) {
+  if (arr.length < 2) return arr;
+  const middleIndex = Math.floor(arr.length / 2);
+  const head = arr.slice(0, middleIndex);
+  const tail = arr.length % 2 ? arr.slice(middleIndex + 1) : arr.slice(middleIndex);
+  return arr.length % 2 ? [...tail, arr[middleIndex], ...head] : [...tail, ...head];
 }
 
 
