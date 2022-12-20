bfd3960d126e5bea838bdf9bc248dda136c06f06
Vladislav Chervyakov <chervyakovve@mail.ru>
2022-12-21 04:17:35 +0700

    feat: add promises solution

--- src/06-promises-tasks.js
+++ src/06-promises-tasks.js
@@ -102,8 +102,14 @@ function getFastestPromise(array) {
  *    });
  *
  */
-function chainPromises(/* array, action */) {
-  throw new Error('Not implemented');
+function chainPromises(array, action) {
+  const result = [];
+  return new Promise((resolve) => {
+    array.forEach((item) => item
+      .then((res) => result.push(res))
+      .catch((err) => new Error(err)));
+    resolve(result);
+  }).then((resolve) => resolve.reduce(action));
 }
 
 module.exports = {
